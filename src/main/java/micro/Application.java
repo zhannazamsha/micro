ab69331c67c0571c65001dc28ea3a77f3ea08d1b
zhanna.zamsha <z.zhanna@gmail.com>
2019-05-17 23:28:46 +0300

    eureka client

--- src/main/java/micro/Application.java
+++ src/main/java/micro/Application.java
@@ -4,21 +4,34 @@ package micro;
 import micro.services.CountryCodesHTMLParsingService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 
 @SpringBootApplication
-    public class Application {
-        public static void main(String[] args) {
-            ApplicationContext context = SpringApplication.run(Application.class, args);
-            CountryCodesHTMLParsingService startService = context.getBean(CountryCodesHTMLParsingService.class);
-            try {
-                startService.collectCountryCodes();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+@EnableEurekaClient
+public class Application {
 
 
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(Application.class, args);
+        CountryCodesHTMLParsingService startService = context.getBean(CountryCodesHTMLParsingService.class);
+        try {
+            startService.collectCountryCodes();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
